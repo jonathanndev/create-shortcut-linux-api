b679571a53c12e7889bbd41e9d48253312eef2a4
jonathan miranda <jonathanmiranda.dev@gmail.com>
2018-06-30 03:25:17 -0300

    initial commit

---
+++ config/config.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var consign = require('consign');
+var bodyParser = require('body-parser'); 
+
+module.exports = function(){
+    var app = express();
+
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+ 
+    consign()
+    .include('controller')
+    .into(app)
+ 
+    return app;
+}
---
+++ controller/createShortcut.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+
+module.exports = function(app){
+
+    app.post('/create-shortcut', function(req, res){
+        var shortcutInfo = req.body;
+
+        var appName = shortcutInfo.appName;
+        var execPath = shortcutInfo.execPath;
+        var iconPath = shortcutInfo.iconPath;
+        var comment = shortcutInfo.comment;
+
+        var lines = '[Desktop Entry]\n' +
+        'Encoding=UTF-8\n' +
+        'Version=1.0\n' +
+        'Type=Application\n' +
+        'Terminal=false\n' +
+        'Name=' + appName + '\n' +
+        'Exec=' + execPath + '\n' +
+        'Icon=' + iconPath + '\n' +
+        'Comment=' + comment + '\n';
+
+        var filePath = './shortcut/' + appName + '.desktop';
+
+        fs.writeFile(filePath, lines, function(err){
+            if(err){
+                throw err;
+            }
+        });
+
+
+    });
+}
